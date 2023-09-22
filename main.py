39f2eec37b220b4519703abf956c132c0bf4d5bf
Nitharshan CV <69662396+nitharshancv@users.noreply.github.com>
2023-09-22 12:33:57 +0530

    Add files via upload
    Project Title: Q-Learning in Pygame: Mastering Pong
    
    ---------------------
    
    In this project, I developed a reinforcement learning agent using Q-learning to master the classic game of Pong, implemented using the Pygame library. The project is organized into three main Python files: `game.py`, `main.py`, and `world.py`, each serving a distinct role in creating a successful learning environment for our AI agent.
    
    1. **game.py**:
       - This file contains the core game logic, utilizing the Pygame framework. It handles the rendering of the Pong game, user input, and the physics of the ball and paddles. The game's graphical interface and interaction are defined here.
    
    2. **main.py**:
       - `main.py` is responsible for parsing command-line arguments and running the entire Q-learning process. It sets up the Q-learning agent, initializes the game environment, and orchestrates the training and evaluation loops.
    
    3. **world.py**:
       - This module serves as the heart of our Q-learning implementation. It encompasses the Q-learning algorithm and its interaction with the game environment. Here are some key components of the Q-learning process in `world.py`:
          - **States**: The state representation is crucial in Q-learning. In this project, states include the x and y positions of the ball and the y-position of the paddle. These states are recorded over a certain number of frames (PHI_LENGTH) to capture the game dynamics effectively.
          - **Rewards**: Rewards are assigned based on specific game situations:
             - Paddle successfully hits the ball: +0.5 reward
             - Agent wins the round: +1 reward
             - Agent loses the round: -1 reward
          - **Actions**: The agent can perform three actions - move up, move down, or stay in the current position. These actions form the agent's action space, defining its possible moves during gameplay.
          - **Q-Learning**: The Q-learning algorithm is implemented to help the agent learn optimal actions to maximize its cumulative rewards over time. It updates the Q-values for state-action pairs based on the rewards obtained during gameplay.
    
    The goal of this project is to train an AI agent using Q-learning to become proficient at playing Pong. Through multiple iterations of gameplay, the agent learns to make decisions that lead to favorable outcomes and maximize its score.
    
    This project is an excellent example of using reinforcement learning to tackle classic video games, demonstrating how a Q-learning agent can adapt and improve its performance through interaction with its environment. It showcases the power of Pygame and reinforcement learning techniques in creating intelligent game-playing agents.

---
+++ main.py
@@ -0,0 +1,71 @@
+# Nitharshan CV_20BCE1732
+# Neeraj S 20BCE1656 
+
+import argparse
+import pickle
+
+from world import Ball, Paddle, World
+
+
+def main(args):
+    Ball.VELOCITY = args.velocity
+    Paddle.VELOCITY = args.velocity
+    Paddle.LENGTH = args.paddle_length
+
+    world = World(args)
+
+    # load saved weights
+    if args.load:
+        Q = pickle.load(open(args.filename, "rb"))
+        world.final_show(Q)
+
+    # train
+    else:
+        Q = world.qlearning()
+        print("Training done!")
+
+        # save Q-values if needed
+        if args.filename is not None:
+            pickle.dump(Q, open(args.filename, "wb"))
+            print("Saved Q in ", args.filename)
+
+        # show final game if needed
+        if args.final_show:
+            input("Press any key to play game...")
+            world.final_show(Q)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # game params
+    parser.add_argument("--canvas_size", nargs=2, default=(32, 24), type=int)
+    parser.add_argument("--paddle_length", default=7, type=int)
+    parser.add_argument("--velocity", default=1, type=int)
+    parser.add_argument("--filename", type=str)
+    parser.add_argument("--load", action="store_true")
+
+    # qlearning params
+    parser.add_argument("--max_iter", default=1000, type=int)
+    parser.add_argument("--learning_rate", default=0.3, type=float)
+    parser.add_argument("--discount", default=0.99, type=float)
+    parser.add_argument("--epsilon", default=0.05, type=float)
+    parser.add_argument("--alpha", default=1.0, type=float)
+    parser.add_argument("--train_episodes", default=1000, type=int)
+    parser.add_argument("--eval_episodes", default=10, type=int)
+    parser.add_argument("--eval_every", default=10, type=int)
+    parser.add_argument("--plot_scores", action="store_true")
+    parser.add_argument("--final_show", action="store_true")
+    parser.add_argument("--fps", default=20, type=int)
+
+    # training strategies
+    parser.add_argument("--agent_strategy", default="eps_greedy", type=str)
+    parser.add_argument("--choose_unexplored_first", action="store_true")
+    parser.add_argument("--opponent_strategy", default="random", type=str)
+    parser.add_argument("--mirror_actions", action="store_true")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main(parse_args())
